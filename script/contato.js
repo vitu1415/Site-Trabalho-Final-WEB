470dbf088a6a0d2f521c6db20504eed646a28e41
Vitor Alves <vitoralves0801@gmail.com>
2024-08-21 17:25:54 -0300

    vs-3

--- script/contato.js
+++ script/contato.js
@@ -1,10 +1,19 @@
 function funcaoContato(formulario){
-    nome = formulario.nome.value;
-    email = formulario.email.value;
-    telefone = formulario.tel.value;
-    dataNascimento = formulario.nascimento.value;
-    sexo = document.getElementsByName("sexo").value;
-    
+    let nome = formulario.nome.value;
+    let email = formulario.email.value;
+    let telefone = formulario.tel.value;
+    let dataNascimento = formulario.nascimento.value;
+    let mas = document.getElementById('masculino');
+    let fem = document.getElementById('feminino');
+
+    if(mas.checked){
+        sexo = mas.value;
+    }else if(fem.checked){
+        sexo = fem.value;
+    }else{
+        sexo = "s/valor";
+    }
+
     document.getElementById("resul").innerHTML=("nome = "+nome+
     "<br>Email = "+email+
     "<br>telefone = "+telefone+
